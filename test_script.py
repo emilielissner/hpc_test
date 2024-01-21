cf1f299fea22db7de46e7e2a67b5e975e6b93033
FuriaAir <lissner94@gmail.com>
2024-01-21 17:16:56 +0100

    Test

---
+++ test_script.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# Create a 2x2 array
+array = np.array([[1, 2], [3, 4]])
+
+# Calculate the determinant
+det = np.linalg.det(array)
+
+print("Array:")
+print(array)
+print("Determinant:", det)
